384ee9b97287b26b4eed72126bcb81fa46f58043
RomainTHD <romain-thd+github@protonmail.com>
2022-02-18 23:30:20 +0100

    test: Fixed chat controller test

--- api/src/chat/chat.controller.spec.ts
+++ api/src/chat/chat.controller.spec.ts
@@ -5,16 +5,32 @@ import {
 import {ChatController} from "chat/chat.controller";
 import {ChatService} from "chat/chat.service";
 
-describe("FsController", () => {
+describe("ChatController", () => {
     let controller: ChatController;
+    let service: ChatService;
 
     beforeEach(async () => {
         const app: TestingModule = await Test.createTestingModule({
             controllers: [ChatController],
-            providers: [ChatService],
+            providers: [{
+                provide: ChatService,
+                useValue: {
+                    getAll: jest.fn(),
+                    getById: jest.fn(),
+                    create: jest.fn(),
+                    update: jest.fn(),
+                    delete: jest.fn(),
+                },
+            }],
         }).compile();
 
+        service    = app.get<ChatService>(ChatService);
         controller = app.get<ChatController>(ChatController);
-        void controller;
+    });
+
+    describe("basic test", () => {
+        it("should be implemented", () => {
+            expect(true).toBeTrue();
+        });
     });
 });
