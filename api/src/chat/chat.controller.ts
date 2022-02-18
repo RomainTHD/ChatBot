e26c79932070379aef3f6fd844a9f295f47fd044
RomainTHD <romain-thd+github@protonmail.com>
2022-02-18 20:15:13 +0100

    feat: Added some endpoints methods

--- api/src/chat/chat.controller.ts
+++ api/src/chat/chat.controller.ts
@@ -1,10 +1,19 @@
 import {
+    Body,
     Controller,
+    Delete,
     Get,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post,
+    Put,
 } from "@nestjs/common";
 import {Chat} from "chat/chat.entity";
 import {ChatService} from "chat/chat.service";
 import {Route} from "com";
+
+// TODO: Replace `Partial<Chat>` with a custom shared interface
 
 @Controller("chat")
 export class ChatController {
@@ -18,5 +27,48 @@ export class ChatController {
     @Route("All chats")
     public async findAll(): Promise<Chat[]> {
         return this.chatService.findAll();
+    }
+
+    @Get(":id")
+    @Route("Chat by id")
+    async findByID(
+        @Param("id", ParseIntPipe) id: string,
+    ): Promise<Chat | null> {
+        return this.chatService.findByID(id);
+    }
+
+    @Post(":id")
+    @Route("Create a chat")
+    async create(
+        @Param("id", ParseIntPipe) id: string,
+        @Body() chat: Partial<Chat>,
+    ): Promise<Chat | null> {
+        return this.chatService.create(chat);
+    }
+
+    @Put(":id")
+    @Route("Update a chat, full")
+    async updateFull(
+        @Param("id", ParseIntPipe) id: string,
+        @Body() chat: Partial<Chat>,
+    ): Promise<Chat | null> {
+        return this.chatService.update(id, chat);
+    }
+
+    @Patch(":id")
+    @Route("Update a chat, partial")
+    async updatePart(
+        @Param("id", ParseIntPipe) id: string,
+        @Body() chat: Partial<Chat>,
+    ): Promise<Chat | null> {
+        return this.chatService.update(id, chat);
+    }
+
+    @Delete(":id")
+    @Route("Delete a chat")
+    async delete(
+        @Param("id", ParseIntPipe) id: string,
+    ): Promise<boolean> {
+        return this.chatService.delete(id);
     }
 }
--- api/src/chat/chat.service.ts
+++ api/src/chat/chat.service.ts
@@ -20,13 +20,48 @@ export class ChatService {
 
     /**
      * Get all chats
-     * @returns Chat[]
+     * @returns All chats
      */
     public async findAll(): Promise<Chat[]> {
         return this.chatRepository.find();
     }
 
-    public getHello(): string {
-        return "Hello World!";
+    /**
+     * Get chat by id
+     * @param id Chat id
+     * @returns Corresponding chat
+     */
+    public async findByID(id: string): Promise<Chat | null> {
+        const chat = await this.chatRepository.findOne(id);
+        return chat ?? null;
+    }
+
+    /**
+     * Create a new chat
+     * @param chat Chat to create
+     * @returns Created chat
+     */
+    public async create(chat: Partial<Chat>): Promise<Chat> {
+        return this.chatRepository.save(chat);
+    }
+
+    /**
+     * Update a chat
+     * @param id Id of the chat to update
+     * @param chat Chat to update
+     * @returns Updated chat
+     */
+    public async update(id: string, chat: Partial<Chat>): Promise<Chat> {
+        return this.chatRepository.save(chat);
+    }
+
+    /**
+     * Delete a chat
+     * @param id Id of the chat to delete
+     * @returns Chat deleted or not
+     */
+    public async delete(id: string): Promise<boolean> {
+        const res = await this.chatRepository.delete(id);
+        return res.affected !== 0;
     }
 }
