ed4d6f916ef93ef68a4aaaec45d47fea7992b636
RomainTHD <romain-thd+github@protonmail.com>
2022-02-19 23:58:34 +0100

    feat: GPT response
    The hyperparameters still need some tuning

--- api/src/chat/chat.controller.ts
+++ api/src/chat/chat.controller.ts
@@ -11,27 +11,46 @@ import {
 } from "@nestjs/common";
 import {ChatService} from "chat/chat.service";
 import {Route} from "com";
+import {GPTBot} from "gpt";
 import {
     ChatDTO_fromClient,
     ChatDTO_toClient,
 } from "shared/dto";
 
-// TODO: Replace `Partial<Chat>` with a custom shared interface
-
+/**
+ * Chat controller
+ */
 @Controller("chat")
 export class ChatController {
+    /**
+     * Chat service
+     * @private
+     */
     private readonly chatService: ChatService;
 
+    /**
+     * Constructor
+     * @param chatService Chat service
+     */
     public constructor(chatService: ChatService) {
         this.chatService = chatService;
     }
 
+    /**
+     * Get all chats
+     * @returns All chats
+     */
     @Get()
     @Route("All chats")
     public async findAll(): Promise<ChatDTO_toClient[]> {
         return this.chatService.findAll();
     }
 
+    /**
+     * Get a chat by its ID
+     * @param id Chat ID
+     * @returns Chat
+     */
     @Get(":id")
     @Route("Chat by id")
     public async findByID(
@@ -40,14 +59,43 @@ export class ChatController {
         return this.chatService.findByID(id);
     }
 
+    /**
+     * Creates a new chat
+     * @param chat Chat to create
+     * @returns Created chat
+     * NOTE: For now, the returned chat is actually the OpenAI response
+     */
     @Post()
     @Route("Create a chat")
     public async create(
-        @Body() chat: Partial<ChatDTO_fromClient>,
+        @Body() chat: ChatDTO_fromClient,
     ): Promise<ChatDTO_toClient | null> {
-        return this.chatService.create(chat);
+        await this.chatService.create(chat);
+
+        const discussion = await this.chatService.getDiscussion(
+            chat.from,
+            chat.to,
+        );
+
+        const botResponse = await GPTBot.generateText(
+            discussion,
+            chat.from,
+            chat.to,
+        );
+
+        return this.chatService.create({
+            from: chat.to,
+            to: chat.from,
+            content: botResponse || "[No response]",
+        });
     }
 
+    /**
+     * Fully replaces the chat with the given ID
+     * @param id Chat ID
+     * @param chat Chat to replace with
+     * @returns The updated chat
+     */
     @Put(":id")
     @Route("Update a chat, full")
     public async updateFull(
@@ -57,6 +105,12 @@ export class ChatController {
         return this.chatService.update(id, chat);
     }
 
+    /**
+     * Partially update a chat
+     * @param id Chat ID
+     * @param chat Partial chat data
+     * @returns Updated chat
+     */
     @Patch(":id")
     @Route("Update a chat, partial")
     public async updatePart(
@@ -66,6 +120,11 @@ export class ChatController {
         return this.chatService.update(id, chat);
     }
 
+    /**
+     * Delete a chat
+     * @param id Chat ID
+     * @returns Chat deleted or not
+     */
     @Delete(":id")
     @Route("Delete a chat")
     public async delete(
