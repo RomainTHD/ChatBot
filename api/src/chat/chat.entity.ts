f8b0341649ca4d5c0d337365ecde8dcdc70e350a
RomainTHD <romain-thd+github@protonmail.com>
2022-02-19 23:59:41 +0100

    fix: Constant chat creation date

--- api/src/chat/chat.entity.ts
+++ api/src/chat/chat.entity.ts
@@ -5,28 +5,52 @@ import {
     PrimaryGeneratedColumn,
 } from "typeorm";
 
+/**
+ * Chat entity
+ */
 @Entity()
 export class ChatEntity {
+    /**
+     * Chat ID
+     */
     @PrimaryGeneratedColumn("uuid")
     public id: string;
 
+    /**
+     * From user ID
+     */
     @Column()
     public from: string;
 
+    /**
+     * To user ID
+     */
     @Column()
     public to: string;
 
+    /**
+     * Message content
+     */
     @Column({
         type: "text",
     })
     public content: string;
 
+    /**
+     * Creation date
+     */
     @Column({
         type: "int",
-        default: Math.floor(Date.now() / 1000),
+        default: 0,
     })
     public createdAt: number;
 
+    /**
+     * Convert to DTO
+     * @param obj Chat object
+     * @returns Chat DTO
+     * @see ChatDTO_toClient
+     */
     public static toDTO(obj: ChatEntity): ChatDTO_toClient {
         return {
             id: obj.id,
